ea5cbc7d8bd198096be32fc1f60dc2fda8c06736
Kristen Casselman <kristen.casselman@yahoo.com>
2018-10-30 14:08:12 -0400

    login component almost complete

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class AuthenticationService {
     constructor(private http: HttpClient) { }
 
     login(email: string, password: string) {
-        return this.http.post<any>(`${config.apiUrl}/users/authenticate`, { email: email, password: password })
+        return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { email: email, password: password })
             .pipe(map(user => {
                 if (user && user.token) {
                     localStorage.setItem('currentUser', JSON.stringify(user));
