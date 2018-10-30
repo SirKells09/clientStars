d027c41ab8b3fe8d416937219c49703e24667756
Kristen Casselman <kristen.casselman@yahoo.com>
2018-10-30 10:05:14 -0400

    message

---
+++ src/app/_models/user.ts
@@ -0,0 +1,10 @@
+import { InternalNgModuleRef } from "@angular/core/src/linker/ng_module_factory";
+
+export class User {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    pin: number;
+    stars: number;
+}
