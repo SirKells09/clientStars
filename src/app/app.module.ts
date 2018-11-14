ee07973746fe977604d1b2e471b3a8842e384067
Kristen Casselman <kristen.casselman@yahoo.com>
2018-11-14 14:21:44 -0500

    kristen updates Wednesday

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material';
 import { HighlightDirective } from './highlight.directive';
-import { JwtInterceptor } from  './_helpers/jwt.interceptor';
 
 
 
